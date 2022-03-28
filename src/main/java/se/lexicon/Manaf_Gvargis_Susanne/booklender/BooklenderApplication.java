6abc0b769921a0d1c0c666a5f10ac769adbf9e78
manaf <manafsayade92@gmail.com>
2022-03-28 10:08:14 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Manaf_Gvargis_Susanne/booklender/BooklenderApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.Manaf_Gvargis_Susanne.booklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/Manaf_Gvargis_Susanne/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.Manaf_Gvargis_Susanne.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
